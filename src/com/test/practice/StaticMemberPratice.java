5d631690bcfeccfac31dfb64d4f8cebec4678561
Anjali <anjalisingh6789@gmail.com>
2021-02-06 22:29:48 +0530

    practice basic

---
+++ src/com/test/practice/StaticMemberPratice.java
@@ -0,0 +1,34 @@
+package com.test.practice;
+
+public class StaticMemberPratice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mains start");
+		Run.display();
+		Run.i=10;	
+		Run.display();
+		System.out.println("Mains end");
+	
+	}
+
+}
+
+ class Run
+{
+	static int i;
+	
+	static void display()
+	{
+		System.out.println("Display()start");
+		System.out.println(i);
+		System.out.println("Display()end");
+		
+	}
+	
+}
+
+
+
+ 
